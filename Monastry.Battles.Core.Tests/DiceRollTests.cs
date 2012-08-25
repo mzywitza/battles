f8eea143ed9ca0d332246ef9f4aa6c90c84ba777
Markus Zywitza <markus.zywitza@gmail.com>
2012-08-25 14:28:03 +0200

    DiceRoll created

---
+++ Monastry.Battles.Core.Tests/DiceRollTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Monastry.Battles.Core
+{
+    [TestFixture]
+    public class DiceRollTests
+    {
+        [Test]
+        public void DiceRollContainsNumber()
+        {
+            var roll = new DiceRoll() {Number = 4};
+            Assert.That(roll.Number, Is.EqualTo(4));
+        }
+    }
+}
---
+++ Monastry.Battles.Core/DiceRoll.cs
@@ -0,0 +1,7 @@
+namespace Monastry.Battles.Core
+{
+    public class DiceRoll
+    {
+        public int? Number { get; set; }
+    }
+}
